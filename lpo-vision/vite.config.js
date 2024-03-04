4d18c973bded631b08297f2031468613099d65cd
lpodev <lpodev@proton.me>
2024-03-04 17:18:43 +0100

    Create Dockerfile for lpo-vision

--- lpo-vision/vite.config.js
+++ lpo-vision/vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 })
