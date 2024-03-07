545c5f01f529199e24a06d612941516f3b5b3836
lpodev <lpodev@proton.me>
2024-03-07 17:37:50 +0100

    Add TheLocaleDropdown unit tests

---
+++ lpo-vision/src/components/__tests__/TheLocaleDropdown.spec.js
@@ -0,0 +1,19 @@
+const { describe, it } = require('vitest')
+
+describe('TheLocaleDropdown', () => {
+  it('renders a select', () => {})
+  it('renders current locale', () => {})
+  it('renders arrow down icon', () => {})
+
+  describe('when activated', () => {
+    it('renders arrow up icon', () => {})
+    it('renders locales options', () => {})
+    describe('when hovering an option', () => {
+      it('hovered option has a darker background', () => {})
+      describe('when option clicked', () => {
+        it('renders selected locale', () => {})
+        it('updates active application locale', () => {})
+      })
+    })
+  })
+})
