bf11b14448f324f395095a0c1b65cd7a79257dbf
lpodev <lpodev@proton.me>
2024-03-08 17:27:48 +0100

    Write TheHeader tests

--- lpo-vision/src/components/ui/__tests__/TheHeader.spec.js
+++ lpo-vision/src/components/ui/__tests__/TheHeader.spec.js
@@ -1,6 +1,19 @@
-const { describe, it } = require('vitest')
+/**
+ * @vitest-environment happy-dom
+ */
+
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import TheHeader from '../TheHeader.vue'
 
 describe('TheHeader', () => {
-  it('renders a title', () => {})
-  it('renders a closed locale dropdown', () => {})
+  it('renders a title', () => {
+    const wrapper = mount(TheHeader)
+    expect(wrapper.find('h1').exists()).toBeTruthy()
+  })
+  it('renders a closed locale dropdown', () => {
+    const wrapper = mount(TheHeader)
+    expect(wrapper.find('li').exists()).toBeTruthy()
+  })
 })
