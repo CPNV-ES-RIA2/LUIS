fb02467058bbf9269d232c38fdbb922084b8989a
lpodev <lpodev@proton.me>
2024-03-08 16:39:08 +0100

    Add AnalyseResult unit tests

---
+++ lpo-vision/src/views/analyses/__tests__/AnalyseResult.spec.js
@@ -0,0 +1,16 @@
+const { describe, it } = require('vitest')
+
+describe('AnalyseResult', () => {
+  it('renders a title', () => {})
+
+  describe('if there are labels', () => {
+    it('renders the labels', () => {})
+    describe('a label', () => {
+      it('renders label name', () => {})
+      it('renders label score', () => {})
+    })
+  })
+  describe('if there are no labels', () => {
+    it("renders 'No labels'", () => {})
+  })
+})
