0c9ff6b0980c1a519e5fc929ddf6b3383dfec2c1
lpodev <lpodev@proton.me>
2024-03-09 11:56:16 +0100

    Code TheHeader

---
+++ lpo-vision/src/components/layout/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <div class="container-md">
+                <a class="navbar-brand" href="#">LPOVISION</a>
+                <the-locale-dropdown></the-locale-dropdown>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+import TheLocaleDropdown from '../TheLocaleDropdown.vue'
+export default {
+    components: {
+        TheLocaleDropdown,
+    },
+}
+</script>
+
+<style scoped></style>
