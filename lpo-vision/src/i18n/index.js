edd7eab136ad7c5fbd09391cb3f4689aabfc1ff0
lpodev <lpodev@proton.me>
2024-03-13 19:14:44 +0100

    Setup i18n to Composition API

--- lpo-vision/src/i18n/index.js
+++ lpo-vision/src/i18n/index.js
@@ -1,26 +1,25 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n';
 
-import en from './languages/en.json'
-import fr from './languages/fr.json'
-import de from './languages/de.json'
-import pt from './languages/pt.json'
+import en from './languages/en.json';
+import fr from './languages/fr.json';
+import de from './languages/de.json';
+import pt from './languages/pt.json';
 
 function loadLocaleMessages() {
-  const locales = [{ en: en }, { fr: fr }, { de: de }, { pt: pt }]
-  const messages = {}
+  const locales = [{ en: en }, { fr: fr }, { de: de }, { pt: pt }];
+  const messages = {};
   locales.forEach((lang) => {
-    const key = Object.keys(lang)
-    messages[key] = lang[key]
-  })
-  return messages
+    const key = Object.keys(lang);
+    messages[key] = lang[key];
+  });
+  return messages;
 }
 
-let browserLocale = navigator.language.split('-')[0]
+let browserLocale = navigator.language.split('-')[0];
 
 export default createI18n({
+  legacy: false,
   locale: browserLocale,
   fallbackLocale: 'en',
-  legacy: false,
-  globalInjection: true,
   messages: loadLocaleMessages(),
-})
+});
