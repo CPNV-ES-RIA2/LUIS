bba4223b9e93b46d7530e84a3771c1f78ded61cf
lpodev <lpodev@proton.me>
2024-03-27 17:49:55 +0100

    Update header link and accept image types in TheAnalyseForm

--- lpo-vision/src/views/analyses/TheAnalyseForm.vue
+++ lpo-vision/src/views/analyses/TheAnalyseForm.vue
@@ -1,43 +1,42 @@
 <template>
-    <div class="card p-3 bg-dark text-white">
-        <div class="card-body">
-            <h2 class="card-title">{{ formHeader }}</h2>
-            <form class="d-flex flex-column justify-content-center gap-3 align-items-center"
-                @submit.prevent="submitForm">
-                <div class="mb-3">
-                    <label for="inputImage" class="form-label">
-                        {{ fileInputLabel }}
-                    </label>
-                    <input class="form-control" type="file" id="inputImage" accept="image/*" ref="fileInput"
-                        @change="onFileSelected" />
-                </div>
-                <div class="d-flex flex-row gap-3">
-                    <div class="mb-3">
-                        <label for="inputMinConfidence" class="form-label">
-                            {{ minConfidenceLabel }}
-                        </label>
-                        <input type="number" id="inputMinConfidence" class="form-control" min="1" max="100"
-                            v-model.number="minConfidence" @change="resetFormValidity" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="inputMaxLabels" class="form-label">
-                            {{ maxLabelsLabel }}
-                        </label>
-                        <input type="number" id="inputMaxLabels" class="form-control" min="1" max="50"
-                            v-model.number="maxLabels" @change="resetFormValidity" />
-                    </div>
-                </div>
-
-                <div class="alert alert-danger" role="alert" v-if="!formIsValid">
-                    The form is not valid. Please check the fields and try again.
-                </div>
-
-                <button type="submit" class="btn btn-primary">
-                    {{ submitButtonText }}
-                </button>
-            </form>
+  <div class="card p-3 bg-dark text-white">
+    <div class="card-body">
+      <h2 class="card-title">{{ formHeader }}</h2>
+      <form class="d-flex flex-column justify-content-center gap-3 align-items-center" @submit.prevent="submitForm">
+        <div class="mb-3">
+          <label for="inputImage" class="form-label">
+            {{ fileInputLabel }}
+          </label>
+          <input class="form-control" type="file" id="inputImage" accept="image/png, image/jpeg, image/jpg"
+            @change="onFileSelected" />
         </div>
+        <div class="d-flex flex-row gap-3">
+          <div class="mb-3">
+            <label for="inputMinConfidence" class="form-label">
+              {{ minConfidenceLabel }}
+            </label>
+            <input type="number" id="inputMinConfidence" class="form-control" min="1" max="100"
+              v-model.number="minConfidence" @change="resetFormValidity" />
+          </div>
+          <div class="mb-3">
+            <label for="inputMaxLabels" class="form-label">
+              {{ maxLabelsLabel }}
+            </label>
+            <input type="number" id="inputMaxLabels" class="form-control" min="1" max="50" v-model.number="maxLabels"
+              @change="resetFormValidity" />
+          </div>
+        </div>
+
+        <div class="alert alert-danger" role="alert" v-if="!formIsValid">
+          The form is not valid. Please check the fields and try again.
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+          {{ submitButtonText }}
+        </button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -54,85 +53,88 @@ const fileInput = ref(null);
 const formIsValid = ref(true);
 
 function onFileSelected(event) {
-    resetFormValidity();
-    const selectedFile = event.target.files[0];
+  resetFormValidity();
+  const selectedFile = event.target.files[0];
 
-    if (isFileValid(selectedFile)) {
-        fileInput.value = selectedFile;
-    } else {
-        alert('Invalid file type or size. Please select a valid image file (png, jpg, jpeg) less than 2MB');
-        fileInput.value = null;
-    }
+  if (isFileValid(selectedFile)) {
+    fileInput.value = selectedFile;
+  } else {
+    alert('Invalid file type or size. Please select a valid image file (png, jpg, jpeg) less than 2MB');
+    fileInput.value = null;
+
+    event.target.value = '';
+  }
 }
 
 function isFileValid(file) {
-    const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-    const maxSize = 2 * 1024 * 1024;
+  const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+  const maxSize = 2 * 1024 * 1024;
 
-    if (!file) {
-        return false;
-    } else if (!allowedTypes.includes(file.type)) {
-        return false;
-    } else if (file.size > maxSize) {
-        return false;
-    }
-    return true;
+  if (!file) {
+    return false;
+  } else if (!allowedTypes.includes(file.type)) {
+    return false;
+  } else if (file.size > maxSize) {
+    return false;
+  }
+  return true;
 }
 
 function maxLabelsIsValid(maxLabelsValue) {
-    if (!maxLabelsValue) {
-        return false;
-    } else if (maxLabelsValue < 1 && maxLabelsValue > 50) {
-        return false;
-    }
-    return true;
+  if (!maxLabelsValue) {
+    return false;
+  } else if (maxLabelsValue < 1 && maxLabelsValue > 50) {
+    return false;
+  }
+  return true;
 }
 
 function minConfidenceIsValid(minConfidenceValue) {
-    if (!minConfidenceValue) {
-        return false;
-    } else if (minConfidenceValue < 1 && minConfidenceValue > 100) {
-        return false;
-    }
-    return true;
+  if (!minConfidenceValue) {
+    return false;
+  } else if (minConfidenceValue < 1 && minConfidenceValue > 100) {
+    return false;
+  }
+  return true;
 }
 
 function resetFormValidity() {
-    formIsValid.value = true;
+  formIsValid.value = true;
 }
 
 function submitForm() {
-    formIsValid.value = true;
-    if (
-        !minConfidenceIsValid(minConfidence.value) ||
-        !maxLabelsIsValid(maxLabels.value) ||
-        !isFileValid(fileInput.value)
-    ) {
-        formIsValid.value = false;
-        return;
-    }
-    const formData = {
-        image: fileInput.value,
-        minConfidence: minConfidence.value,
-        maxLabels: maxLabels.value,
-    };
-    emits('form-submitted', formData);
+  formIsValid.value = true;
+  if (
+    !minConfidenceIsValid(minConfidence.value) ||
+    !maxLabelsIsValid(maxLabels.value) ||
+    !isFileValid(fileInput.value)
+  ) {
+    formIsValid.value = false;
+    return;
+  }
+  const formData = {
+    image: fileInput.value,
+    minConfidence: minConfidence.value,
+    maxLabels: maxLabels.value,
+  };
+
+  emits('form-submitted', formData);
 }
 
 // i18n translations
 const formHeader = computed(() => {
-    return t('formHeader');
+  return t('formHeader');
 });
 const fileInputLabel = computed(() => {
-    return t('fileInputLabel');
+  return t('fileInputLabel');
 });
 const minConfidenceLabel = computed(() => {
-    return t('minConfidenceLabel');
+  return t('minConfidenceLabel');
 });
 const maxLabelsLabel = computed(() => {
-    return t('maxLabelsLabel');
+  return t('maxLabelsLabel');
 });
 const submitButtonText = computed(() => {
-    return t('submitButton');
+  return t('submitButton');
 });
 </script>
